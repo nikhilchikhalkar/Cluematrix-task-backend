d34ea6a9c42a3662add5ad840fec91712e7faff8
NIKHIL CHIKHALKAR <155292217+nikhilchikhalkar@users.noreply.github.com>
2025-07-24 17:28:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-    origin: ['http://localhost:3000'], 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
